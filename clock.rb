a91c6577d2512636e2cbcdf5c590644cc39158ba
Vorachith Kip <songta17@hotmail.com>
2021-12-17 23:49:44 +0100

    spec 24 : test_add_more_than_one_hour done

--- clock.rb
+++ clock.rb
@@ -4,7 +4,7 @@ class Clock
   attr_reader :hour, :minute
 
   # clock = Clock.new(hour: 10, minute: 0)
-  #   assert_equal "10:03", (clock + Clock.new(minute: 3)).to_s
+  # assert_equal "11:01", (clock + Clock.new(minute: 61)).to_s
   
   def initialize(hour:0, minute:0)
     @hour = (hour + minute / 60)% 24
@@ -17,6 +17,7 @@ class Clock
 
   def +(other)
     minute_new = @minute + other.minute
-    Clock.new(hour: @hour, minute: minute_new)
+    hour_new = @hour + other.hour
+    Clock.new(hour: hour_new, minute: minute_new)
   end
 end
